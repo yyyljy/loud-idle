4368b15b409a9eb452b609cfbe2786ed3ded882a
yyyljy <yyyljy@gmail.com>
2022-10-28 20:46:31 +0900

    20221028

--- src/components/KakaoAuth.jsx
+++ src/components/KakaoAuth.jsx
@@ -19,23 +19,24 @@ function KakaoAuth() {
         setError(null);
         setData(null);
         setLoading(true);
-        const config = {
-          method: "POST",
-          url: "https://kauth.kakao.com/oauth/token",
-          data: qs.stringify({
-            grant_type: "authorization_code",
-            client_id: process.env.REACT_APP_KAKAO_RESTAPI_KEY,
-            redirect_uri: process.env.REACT_APP_KAKAO_REDIRECT_URI,
-            code: codeRecv,
-          }),
-        };
-        await axios(config).then((res) => {
-          setData(res.data);
-          console.log(data);
-        });
       }
+      const config = {
+        method: "POST",
+        url: "https://kauth.kakao.com/oauth/token",
+        data: qs.stringify({
+          grant_type: "authorization_code",
+          client_id: process.env.REACT_APP_KAKAO_RESTAPI_KEY,
+          redirect_uri: process.env.REACT_APP_KAKAO_REDIRECT_URI,
+          code: codeRecv,
+        }),
+      };
+      await axios(config).then((res) => {
+        setData(res.data);
+        console.log(data);
+      });
     } catch (e) {
       setError(e);
+      console.log(e);
     }
     setLoading(false);
   };
