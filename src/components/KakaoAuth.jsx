71d5b8afc929a1929a8e7687d04e4e05eeee6755
yyyljy <yyyljy@gmail.com>
2022-10-31 23:57:33 +0900

    20221031

--- src/components/KakaoAuth.jsx
+++ src/components/KakaoAuth.jsx
@@ -91,6 +91,7 @@ function KakaoAuth() {
   // };
 
   useEffect(() => {
+    console.log("test3");
     let codeCookie = document.cookie.split("authorize-access-token=");
     let code = "";
     if (codeCookie.length === 2) {
